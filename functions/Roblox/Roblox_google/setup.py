ebcede9806e593f979451f9a2646c4de50e5b089
Willianz4z4 <willianz4z4@bot.com>
2026-08-21 15:36:53 -0300

    Autoupdate via auto_github

---
+++ functions/Roblox/Roblox_google/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from Cython.Build import cythonize
+
+directives = {
+    'language_level': "3",
+    'bind_c_methods': False
+}
+
+setup(ext_modules=cythonize("phone_assistance.py", compiler_directives=directives))
